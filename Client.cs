7b1144b567ce03d3584cb8dba664e4c3277d0722
DarknessRisesFromBelow <spam.me.please.do.it@gmail.com>
2022-03-17 06:45:51 -0700

    added program.cs and updated client

--- Client.cs
+++ Client.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.Win32;
 using System.Net;
 using System.IO;
 
@@ -6,27 +7,25 @@ namespace TestApplication
 {
     class Connect
     {
-
         public static string GET(string ip, string options, int version)
         {
-            string uri = ip + "/" + options + version;
-            return this.HttpGet(uri);    
+            string uri = "http://" + ip + ":8080"+"/" + options + version;
+            return HttpGet(uri);    
         } 
 
-        public string HttpGet(string URI)
+        static string HttpGet(string remoteUri)
         {
             WebClient client = new WebClient();
 
             // Add a user agent header in case the 
             // requested URI contains a query.
 
+            
+
             client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
 
-            Stream data = client.OpenRead(URI);
-            StreamReader reader = new StreamReader(data);
-            string s = reader.ReadToEnd();
-            data.Close();
-            reader.Close();
+            byte[] data = client.DownloadData (remoteUri);
+            string s = data.ToString();
 
             return s;
         }
@@ -34,17 +33,17 @@ namespace TestApplication
 
     class Client
     {
-    	static void Main(string[] args)
+    	public string Execute()
     	{
-    		string ip = "insert your IP here";
+    		string ip = "your server's IP here";
     		string options = "Update";
-    		RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Version");  
+    		RegistryKey ParentKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Version");  
         	int version = 0;
 			//if it does exist, retrieve the stored values  
-			if (key != null)  
+			if (ParentKey != null)  
 			{  
-    			version = key.GetValue("version");  
-    			key.Close();  
+    			version = int.Parse(ParentKey.GetValue("version").ToString());  
+    			ParentKey.Close();  
 			} 
 			// otherwise, store 0 as version and move on
 			else
@@ -54,7 +53,9 @@ namespace TestApplication
 				version = 0;
 			}
 			// Send a Get Request using a method made above
-    		Connect.GET(ip, options, version);
+    		return (Connect.GET(ip, options, version));
+           
+       
     	}
     }
 }
---
+++ program.cs
@@ -0,0 +1,5 @@
+using TestApplication;
+
+Client c = new Client();
+Console.Write(c.Execute());
+
